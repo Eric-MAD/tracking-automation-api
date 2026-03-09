5e710f3e4500a3eb40250d42c88212d45dc64d52
abrahamprinceramarosandy-code <abraham.prince.ramarosandy@gmail.com>
2026-03-09 14:48:08 +0300

    feat : add create templates service

--- src/services/templates.services.js
+++ src/services/templates.services.js
@@ -3,3 +3,10 @@ import * as templateRepository from "../repositories/templates.repository.js";
 export const getAllTemplates = async () => {
   return templateRepository.getAllTemplates();
 };
+
+export const getTemplateById = async (id) => {
+  return templateRepository.getTemplateById(id);
+}
+export const createTemplate = async (templateData) => {
+  return templateRepository.createTemplate(templateData);
+}
