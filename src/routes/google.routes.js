ef3e6da4e4bb01ff9fb9f401dbe8ebc6584a67ca
abrahamprinceramarosandy-code <abraham.prince.ramarosandy@gmail.com>
2026-03-16 15:57:18 +0300

    feat : create conversion linker and add some get tags and triggers

--- src/routes/google.routes.js
+++ src/routes/google.routes.js
@@ -1,8 +1,12 @@
 import express from "express"
 import { listGoogleAccounts } from "../controllers/googleListAccount.controller.js"
+import { createTestTag, getTagsDiagnostic, getTriggersDiagnostic } from "../controllers/googleTagManager.controller.js"
 
 const googleRouter = express.Router()
 
 googleRouter.get("/accounts", listGoogleAccounts)
+googleRouter.post("/tags", createTestTag);
+googleRouter.get("/tags/diagnostic", getTagsDiagnostic);
+googleRouter.get("/triggers/diagnostic", getTriggersDiagnostic);
 
 export default googleRouter;
